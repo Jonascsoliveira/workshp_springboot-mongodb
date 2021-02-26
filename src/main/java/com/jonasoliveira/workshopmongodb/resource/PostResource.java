b574efdac5c27a6eba875c2abefb4ff56b0fe78a
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-02-25 21:16:34 -0300

    Consulta feita com paramentros

--- src/main/java/com/jonasoliveira/workshopmongodb/resource/PostResource.java
+++ src/main/java/com/jonasoliveira/workshopmongodb/resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.jonasoliveira.workshopmongodb.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jonasoliveira.workshopmongodb.domain.Post;
+import com.jonasoliveira.workshopmongodb.resource.utils.URL;
 import com.jonasoliveira.workshopmongodb.services.PostService;
 
 @RestController
@@ -23,4 +27,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
